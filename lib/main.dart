04e61d2f60458c0f40e90610a1223e7e2fc2d8de
Ruthish <ruthishkumar1711@gmail.com>
2023-04-19 11:34:34 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:portfolio/view/portfolio/home.page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const Homepage(),
+    );
+  }
+}
